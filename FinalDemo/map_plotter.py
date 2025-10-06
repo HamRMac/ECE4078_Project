ed7dccf671548a4f3596c46b1f0eefcf8e21928e
tintagel <jgil0016@student.monash.edu>
2025-10-07 10:42:38 +1100

    Minor changes.

--- FinalDemo/map_plotter.py
+++ FinalDemo/map_plotter.py
@@ -2,7 +2,7 @@ import json
 import matplotlib.pyplot as plt
 
 # Load map from file
-with open("lib_map1.txt", "r") as f:
+with open("true_map.txt", "r") as f:
     map_data = json.load(f)
 
 # Separate markers and fruits
